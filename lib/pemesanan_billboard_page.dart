1ab340fa4f1af630dca03a386510fce87725b63f
alifhilmi17 <muhammadalifhilmi801@gmail.com>
2023-11-16 01:17:38 +0700

    pemesanan_billboard_page UPDATED

--- lib/pemesanan_billboard_page.dart
+++ lib/pemesanan_billboard_page.dart
@@ -21,106 +21,109 @@ class _PemesananBillboardPageState extends State<PemesananBillboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pemesanan Billboard'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Container(
-                width: 400,
-                height: 250,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Image.asset(
-                        widget.imagePath,
-                        width: 380,
-                        height: 100, // Adjust the height of the image
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
-                      child: Text(
-                        widget.catalogContent,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Pembayaran:',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    buildPaymentOptionRadio(1, 'COD'),
-                    Text(
-                      'Transfer Bank:',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    buildPaymentOptionRadio(2, 'Jenis Pembayaran 1'),
-                    buildPaymentOptionRadio(3, 'Jenis Pembayaran 2'),
-                    buildPaymentOptionRadio(4, 'Jenis Pembayaran 3'),
-                  ],
-                ),
-              ),
-            ),
-            Spacer(),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Do something with booking data and selected payment option
-                  print(
-                      'Billboard: ${widget.jenisBillboard}, Pesan Sekarang, Jenis Pembayaran: $selectedPaymentOption');
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 16), // Add horizontal padding as needed
-                  minimumSize: Size(MediaQuery.of(context).size.width,
-                      0), // Set minimum size to full width
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Pesan Sekarang',
-                      style: TextStyle(
-                          fontSize: 18), // Adjust the font size as needed
-                    ),
-                    Spacer(), // Add space between "Pesan Sekarang" and "Harga"
-                    Text(
-                      'Rp.200.000',
-                      style: TextStyle(
-                          fontSize: 18), // Adjust the font size as needed
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Pemesanan Billboard'),
         ),
-      ),
-    );
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Container(
+                  width: 400,
+                  height: 300,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Image.asset(
+                          widget.imagePath,
+                          width: 380,
+                          height: 120, // Adjust the height of the image
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 30.0),
+                        // Increase the top padding to move the text down
+                        child: Text(
+                          widget.catalogContent,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Pembayaran:',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      buildPaymentOptionRadio(1, 'COD'),
+                      Text(
+                        'Transfer Bank:',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      buildPaymentOptionRadio(2, 'Jenis Pembayaran 1'),
+                      buildPaymentOptionRadio(3, 'Jenis Pembayaran 2'),
+                      buildPaymentOptionRadio(4, 'Jenis Pembayaran 3'),
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              Center(
+                child: ElevatedButton(
+                  onPressed: selectedPaymentOption != 0
+                      ? () {
+                          // Do something with booking data and selected payment option
+                          print(
+                              'Billboard: ${widget.jenisBillboard}, Pesan Sekarang, Jenis Pembayaran: $selectedPaymentOption');
+                        }
+                      : null, // Disable the button if no payment option is selected
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    minimumSize: Size(
+                      MediaQuery.of(context).size.width,
+                      0,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Pesan Sekarang',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        'Rp.200.000',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ])));
   }
 
   Widget buildPaymentOptionRadio(int value, String label) {
