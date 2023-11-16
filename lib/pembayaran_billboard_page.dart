8250df810f7e9e0b30a2f41c99b710542ccc4fc2
alifhilmi17 <muhammadalifhilmi801@gmail.com>
2023-11-16 23:52:11 +0700

    halaman pembayaran_billboard_page

---
+++ lib/pembayaran_billboard_page.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import paket intl untuk format tanggal
+
+class PaymentPage extends StatelessWidget {
+  final String jenisBillboard;
+  final int selectedPaymentOption;
+  final String orderPrice;
+  final String orderCode; // Kode pemesanan
+
+  PaymentPage({
+    required this.jenisBillboard,
+    required this.selectedPaymentOption,
+    required this.orderPrice,
+  }) : orderCode = generateOrderCode(
+          jenisBillboard,
+        ); // Panggil fungsi untuk menghasilkan kode pemesanan
+
+  // Fungsi untuk menghasilkan kode pemesanan
+  static String generateOrderCode(String jenisBillboard) {
+    // Contoh: Billboard_TYPE_20230201_123456
+    return 'Billboard_${jenisBillboard}_${DateFormat('yyyyMMdd_HHmmss').format(DateTime.now())}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Pembayaran'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Kode Pemesanan:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    orderCode,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(
+            'Dimohon untuk menuliskan kode ini \npada keterangan transfer',
+            style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+          ),
+          Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Pembeli:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  // Ganti komentar dengan detail pembeli
+                  Text(
+                    'Nama Pembeli: John Doe\nEmail: john@example.com\nNomor Telepon: +1 123-456-7890',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Penjual:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  // Ganti komentar dengan detail penjual
+                  Text(
+                    'Nama Penjual: Seller Name\nEmail: seller@example.com\nNomor Telepon: +1 987-654-3210',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Total Pembayaran:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    orderPrice,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/pemesanan_billboard_page.dart
+++ lib/pemesanan_billboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pembayaran_billboard_page.dart';
 
 class PemesananBillboardPage extends StatefulWidget {
   final String jenisBillboard;
@@ -21,109 +22,120 @@ class _PemesananBillboardPageState extends State<PemesananBillboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Pemesanan Billboard'),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Container(
-                  width: 400,
-                  height: 300,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Image.asset(
-                          widget.imagePath,
-                          width: 380,
-                          height: 120, // Adjust the height of the image
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 30.0),
-                        // Increase the top padding to move the text down
-                        child: Text(
-                          widget.catalogContent,
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+      appBar: AppBar(
+        title: Text('Pemesanan Billboard'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
-              Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Pembayaran:',
-                        style: TextStyle(fontSize: 18),
+              child: Container(
+                width: 400,
+                height: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset(
+                        widget.imagePath,
+                        width: 380,
+                        height: 120,
+                        fit: BoxFit.cover,
                       ),
-                      buildPaymentOptionRadio(1, 'COD'),
-                      Text(
-                        'Transfer Bank:',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      buildPaymentOptionRadio(2, 'Jenis Pembayaran 1'),
-                      buildPaymentOptionRadio(3, 'Jenis Pembayaran 2'),
-                      buildPaymentOptionRadio(4, 'Jenis Pembayaran 3'),
-                    ],
-                  ),
-                ),
-              ),
-              Spacer(),
-              Center(
-                child: ElevatedButton(
-                  onPressed: selectedPaymentOption != 0
-                      ? () {
-                          // Do something with booking data and selected payment option
-                          print(
-                              'Billboard: ${widget.jenisBillboard}, Pesan Sekarang, Jenis Pembayaran: $selectedPaymentOption');
-                        }
-                      : null, // Disable the button if no payment option is selected
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    minimumSize: Size(
-                      MediaQuery.of(context).size.width,
-                      0,
                     ),
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Pesan Sekarang',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 30.0),
+                      child: Text(
+                        widget.catalogContent,
+                        style: TextStyle(fontSize: 16),
                       ),
-                      Spacer(),
-                      Text(
-                        'Rp.200.000',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ])));
+            ),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Pembayaran:',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    buildPaymentOptionRadio(1, 'COD'),
+                    Text(
+                      'Transfer Bank:',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    buildPaymentOptionRadio(2, 'Jenis Pembayaran 1'),
+                    buildPaymentOptionRadio(3, 'Jenis Pembayaran 2'),
+                    buildPaymentOptionRadio(4, 'Jenis Pembayaran 3'),
+                  ],
+                ),
+              ),
+            ),
+            Spacer(),
+            Center(
+              child: ElevatedButton(
+                onPressed: selectedPaymentOption != 0
+                    ? () {
+                        // Navigate to the payment page with the necessary data
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PaymentPage(
+                              jenisBillboard: widget.jenisBillboard,
+                              selectedPaymentOption: selectedPaymentOption,
+                              orderPrice: 'Rp.200.000',
+                            ),
+                          ),
+                        );
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  minimumSize: Size(
+                    MediaQuery.of(context).size.width,
+                    0,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'Pesan Sekarang',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Rp.200.000',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget buildPaymentOptionRadio(int value, String label) {
